39cce06909d119363d3d6149ed52f136c0339114
s0daa <sodaconsumer@gmail.com>
2026-09-20 08:13:04 -0700

    remove dupes in prim\lua

--- Primordial/lua/dumped_RGB Fire Modulation (143).lua
+++
@@ -1,3 +0,0 @@
--- Downloaded from https://github.com/s0daa/CSGO-HVH-LUAS
-
-https://primordial.dev/resources/rainbow-accent-color.13/
--- Primordial/lua/dumped_Watermark remover (477).lua
+++
@@ -1,6 +0,0 @@
--- Downloaded from https://github.com/s0daa/CSGO-HVH-LUAS
-
-local function on_draw_watermark(watermark_text)
-    return ""
-end
-callbacks.add(e_callbacks.DRAW_WATERMARK, on_draw_watermark)
